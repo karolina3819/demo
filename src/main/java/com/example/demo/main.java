408de69ce055dc9255227f2c3b7c842708c83de4
mechanik <karolina3819@gmail.com>
2022-06-25 17:28:38 +0200

    2

--- src/main/java/com/example/demo/main.java
+++ src/main/java/com/example/demo/main.java
@@ -1,4 +1,18 @@
 package com.example.demo;
 
-public class main {
-}
+public main throws IOException {
+    String url ="http://localgost:8080/logowanie";
+    URL obj= new URL(url);
+    HttpURLConnection connection =(HttpURLConnection) obj.openConnection();
+    connection.setRequestProperty("User-Agent","Test");
+
+    Bufferreader in =new Buffereader(
+          new Input
+            String inputline;
+            StringBuffer odpowiedz =new Stringbuffer();
+            while((inputline=in.readLine())!=null){
+            odpowiedz.append(inputLine);
+            odpowiedz.toString();
+            }
+        }
+        )
